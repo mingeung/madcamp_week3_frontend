8daf98e7fd83e7e6355863f3e66a22f07aaaa5dd
mingeung <jmk2229@naver.com>
2024-01-15 16:25:27 +0900

    로그인, 회원가입 페이지 디자인

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -9,6 +9,8 @@ const SignUp = () => {
   const [inputNickName, setInputNickName] = useState("");
   const [inputSignPw, setInpuSigntPw] = useState("");
   const [inputReSignPw, setInputReSignPw] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+  const [responseData, setResponseData] = useState("");
 
   //input data의 변화가 있을 때마다 value 값을 변경해서 useState 해준다.
   const handleInputSignId = (e) => {
@@ -24,20 +26,48 @@ const SignUp = () => {
   const handleInputReSignPw = (e) => {
     setInputReSignPw(e.target.value);
   };
+  const handleInputEmail = (e) => {
+    setInputEmail(e.target.value);
+  };
 
   //회원가입 버튼 클릭 이벤트
-  const onClickSignUp = () => {
+  const onClickSignUp = async () => {
     console.log("click SignUp");
+
+    try {
+      console.log("try 실행");
+      const response = await axios.post("http://172.10.7.24:80/register", {
+        email: inputEmail,
+        user_id: inputSignId,
+        nickname: inputNickName,
+        password: inputSignPw,
+      });
+      console.log("await 실행");
+      setResponseData(response.data);
+      console.log(response.data);
+    } catch (e) {
+      console.log("오류 발생:", e);
+    }
   };
 
   return (
-    <div className="sign-up">
-      <div className="signup-container-box">
+    <div className="signup">
+      <div className="total-container">
         <h2 className="signup-title">회원가입</h2>
         <div className="signup-container">
           <div>
             <input
-              className="sign-up-input-box"
+              className="signup-input-box"
+              type="text"
+              name="input_email"
+              placeholder="이메일"
+              value={inputEmail}
+              onChange={handleInputEmail}
+            />
+          </div>
+          <div>
+            <input
+              className="signup-input-box"
               type="text"
               name="input_id"
               placeholder="아이디"
@@ -47,7 +77,7 @@ const SignUp = () => {
           </div>
           <div>
             <input
-              className="sign-up-input-box"
+              className="signup-input-box"
               type="text"
               name="input_nickname"
               placeholder="닉네임"
@@ -57,7 +87,7 @@ const SignUp = () => {
           </div>
           <div>
             <input
-              className="sign-up-input-box"
+              className="signup-input-box"
               type="password"
               name="input_pw"
               placeholder="비밀번호"
@@ -68,7 +98,7 @@ const SignUp = () => {
 
           <div>
             <input
-              className="sign-up-input-box"
+              className="signup-input-box"
               type="password"
               name="input_re_pw"
               placeholder="비밀번호 확인"
@@ -77,7 +107,11 @@ const SignUp = () => {
             />
           </div>
           <div>
-            <button className="btn-pw" type="button" onClick={onClickSignUp}>
+            <button
+              className="signup-button"
+              type="button"
+              onClick={onClickSignUp}
+            >
               회원가입
             </button>
           </div>
