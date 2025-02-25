27a219c215a000291fbddbafa6db5ddb33c25afc
mingeung <jmk2229@naver.com>
2025-02-25 14:10:10 +0900

    Feat : 정지하고 해당 구간에서 재실행하기

--- src/hooks/useMusicPlayer.js
+++ src/hooks/useMusicPlayer.js
@@ -5,23 +5,30 @@ import usePlayerStore from "../store/usePlayerStore.js";
 
 export default function useMusicPlayer(track) {
   const [isPlaying, setIsPlaying] = useState(false);
-  const { deviceId, currentTrack, setCurrentTrack, player } = usePlayerStore();
-
+  const [nowPosition, setNowPosition] = useState(0);
+  const { deviceId, currentTrack, setCurrentTrack, player, position } =
+    usePlayerStore();
   //시작
   const handlePlayStart = async (track) => {
     try {
-      await instance.put(`/playStart/${deviceId}`, {
-        uris: track.uri,
-      });
-      console.log("음원재생");
+      if (track === currentTrack) {
+        await instance.put(`/playStart/${deviceId}`, {
+          uris: track.uri,
+          position_ms: position,
+        });
+      } else {
+        await instance.put(`/playStart/${deviceId}`, {
+          uris: track.uri,
+          // position_ms: position,
+        });
+      }
+
       setIsPlaying(true);
       userMusicSave(track);
       userMusicQueueSave(track, deviceId);
       setCurrentTrack(track); //현재 재생중인 목록 저장
 
       if (player) {
-        console.log("플레이어 상태 변경 이벤트 리스너 추가");
-        // 음악 시작 시 상태 변경 이벤트 리스너 추가
         player.on("player_state_changed", (state) => {
           if (state?.paused) {
             setIsPlaying(false); // 음악이 끝나면 isPlaying을 false로 설정
@@ -39,7 +46,6 @@ export default function useMusicPlayer(track) {
       await instance.put(`/playPause/${deviceId}`);
       setCurrentTrack(track);
       setIsPlaying(false);
-      console.log("음원 정지");
     } catch (error) {
       console.log("음원 정지하기 실패:", error);
     }
